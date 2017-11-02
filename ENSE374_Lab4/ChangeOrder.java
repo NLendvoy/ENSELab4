6accf9a95b7202b9a70d6f05da26361e922b91b8
NLendvoy <32944739+NLendvoy@users.noreply.github.com>
2017-11-02 16:21:20 -0600

    Add files via upload

---
+++ ENSE374_Lab4/ChangeOrder.java
@@ -0,0 +1,4 @@
+
+public class ChangeOrder extends OrderApplication {
+
+}
--- ENSE374_Lab4/Main.java
+++ ENSE374_Lab4/Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Main 
 {
 	public static void main(String[] args) 
 	{
+		Scanner in = new Scanner(System.in);
 		
 		Customer customer = new Customer("Nikolas Lendvoy", "3141 Circle drive", 0.0f, 0.0f);
 		double price = 0.0;
@@ -12,7 +15,23 @@ public class Main
 		if (application.createOrder(order)) 
 		{
 			price = order.calculatePrice();
-			System.out.printf("Price of the order: $%.1f\n", price);
+			System.out.printf("Price of the order: $%.2f\n", price);
+			System.out.printf("Would you like to edit your order? (yes = 0) \n");
+			int input1 = in.nextInt();
+			
+			if (input1 == 0)
+			{
+			
+				System.out.printf("Please enter the products ID you would like to change.\n");
+				int input2 = in.nextInt();
+				
+				System.out.printf("Please enter the quantity desired.\n");
+				int input3 = in.nextInt();	
+				
+				order.editOrderItem(input2, input3);
+				price = order.calculatePrice();
+				System.out.printf("Price of the order: $%.2f\n", price);
+			}
 		}
 	}
 }
